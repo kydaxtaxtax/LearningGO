5675adb33e63e1976c436d57cf30a60f9c0c5839
Kydaxtaxtax <hysgvarnaa@gmail.com>
2021-11-18 18:26:01 +0300

    Lesson 5 Connection MySQL and print table user

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
 import ("fmt" //доступ к базовым функциям
+  "database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type User struct {
+  Name string `json: "name"`
+  Age uint16 `json: "age"`
+}
 
 func main() {
-  fmt.Println("Работа с MySQL")
+  db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/golang")
+  if err != nil {
+    panic(err)
+  }
+
+  defer db.Close()
+
+  //Установка данных
+  // insert, err := db.Query("INSERT INTO `users` (`name`, `age`) VALUES ('Bob', 35)")
+  // if err != nil {
+  //   panic(err)
+  // }
+  // defer insert.Close()
+
+//Выборка данных
+res, err := db.Query("SELECT `name`, `age` FROM `users`")
+if err != nil {
+  panic(err)
+}
+
+for res.Next() {
+    var user User
+    err = res.Scan(&user.Name, &user.Age)
+    if err != nil {
+      panic(err)
+    }
+
+    fmt.Println(fmt.Sprintf("User: %s with age %d", user.Name, user.Age))
+  }
 }
